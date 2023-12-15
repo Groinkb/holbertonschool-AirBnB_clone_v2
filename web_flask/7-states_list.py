401321dcc3b8bfe067e19a9975eb8b45dd03c763
Groinkb <benjamin@mossu.ch>
2023-12-15 17:41:21 +0100

    sd

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Write a script that starts a Flask web application"""
-
+"""This function does nothing."""
 
 from flask import Flask, render_template
 from models import storage
