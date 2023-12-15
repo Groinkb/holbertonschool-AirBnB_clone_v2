fad0b8e62f84940572856687e7a5b28cc9e0cb99
Groinkb <benjamin@mossu.ch>
2023-12-15 17:35:39 +0100

    add

--- web_flask/models/state.py
+++ web_flask/models/state.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-import models
+"""class State that inherits from BaseModel"""
+
+
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import ForeignKey
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import models
+from os import getenv
 from models.city import City
-import os
+import shlex
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-
+    """This is the class for State
+    Attributes:
+        name: input name.
+    """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                          cascade="delete")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", cascade="all, delete", backref="state",
+                              passive_deletes=True)
+    else:
 
-    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            new_ls = []
-            for city in models.storage.all("City").values():
-                if self.id == city.state_id:
-                    new_ls.append(city)
-            return new_ls
+            """Return the list of City instances with state_id
+            equals to the current State.id"""
+            from models import storage
+            state_cities = []
+            all_cities = storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    state_cities.append(city)
+            return state_cities
